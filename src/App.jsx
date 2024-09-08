fc62973f6083974231ba4365c6b2a35e2f833cbd
PSO01 <rndrmrqkaqka@naver.com>
2024-09-08 17:27:31 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const App = () => {
+    return(
+        <>
+           <Header>제목</Header> 
+        </>
+    );
+};
+export default App;
+
+
+const Header = styled.h1`
+    color: red;  
+    font-size: 25px;
+    font-weight: 800;
+`
