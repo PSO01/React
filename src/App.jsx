bc2461edc8d7b271e6d51f0be1684ccfb114fa2a
PSO01 <rndrmrqkaqka@naver.com>
2024-09-22 17:27:02 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 
-
-const App = () => {
-    return(
-        <>
-           <Header>제목</Header> 
-        </>
+const Counter = () => {
+   
+    const [state, setState] = useState(0);
+  
+    return (
+      <div>
+        <h1>State 값 : {state}</h1>
+        {/* setState를 사용하여 state의 값을 1씩 증가시킴 */}
+        <button onClick={() => setState(state + 1)}>1씩 증가</button>
+      </div>
     );
-};
-export default App;
-
-
-const Header = styled.h1`
-    color: red;  
-    font-size: 25px;
-    font-weight: 800;
-`
+  };
